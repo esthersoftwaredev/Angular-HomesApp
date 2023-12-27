f9d8ca4259f88592ecfde2b73c8f53f93667f190
Esther White <monacodelisa@gmail.com>
2023-12-27 22:53:56 +0200

    preparing few things

--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
+import { HousingLocation } from '../../models/housing-location';
 
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.scss'
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
