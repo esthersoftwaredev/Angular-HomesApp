69aeca1baa156bc7fb606af45421e3646fea9590
Esther White <monacodelisa@gmail.com>
2023-12-27 22:38:50 +0200

    one more component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
+import { Component } from "@angular/core";
+import { HousingLocationComponent } from "../housing-location/housing-location.component";
+
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [MatInputModule, MatIconModule],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+	selector: "app-home",
+	standalone: true,
+	imports: [HousingLocationComponent, MatInputModule, MatIconModule],
+	templateUrl: "./home.component.html",
+	styleUrl: "./home.component.scss",
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
