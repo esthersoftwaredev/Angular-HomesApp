6ce6ac1c40cf9a21373c48f2095216ced9e4219e
Esther White <monacodelisa@gmail.com>
2023-12-28 10:20:22 +0200

    basic routing and details comoponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from "./components/home/home.component";
 
-import {MatToolbarModule} from '@angular/material/toolbar';
+import { MatToolbarModule } from "@angular/material/toolbar";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent, MatToolbarModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+	selector: "app-root",
+	standalone: true,
+	imports: [
+		CommonModule,
+		RouterLink,
+		RouterLinkActive,
+		RouterOutlet,
+		HomeComponent,
+		MatToolbarModule,
+	],
+	templateUrl: "./app.component.html",
+	styleUrl: "./app.component.scss",
 })
 export class AppComponent {
-  title = 'homes-app';
+	title = "homes-app";
 }
--- src/app/components/housing-location/housing-location.component.ts
+++ src/app/components/housing-location/housing-location.component.ts
@@ -1,16 +1,23 @@
-import { Component, Input } from '@angular/core';
-import { HousingLocation } from '../../models/housing-location';
+import { Component, Input } from "@angular/core";
+import { RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { HousingLocation } from "../../models/housing-location";
 
-import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-housing-location',
-  standalone: true,
-  imports: [MatCardModule, MatIconModule],
-  templateUrl: './housing-location.component.html',
-  styleUrl: './housing-location.component.scss'
+	selector: "app-housing-location",
+	standalone: true,
+	imports: [
+		RouterLink,
+		RouterLinkActive,
+		RouterOutlet,
+		MatCardModule,
+		MatIconModule,
+	],
+	templateUrl: "./housing-location.component.html",
+	styleUrl: "./housing-location.component.scss",
 })
 export class HousingLocationComponent {
-  @Input() housingLocation!: HousingLocation;
+	@Input() housingLocation!: HousingLocation;
 }
