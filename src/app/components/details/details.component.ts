52f40b20fe643c199506d9527edcbe1f13ba7481
Esther White <monacodelisa@gmail.com>
2023-12-28 12:24:28 +0200

    details page styling

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,21 +1,27 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { HousingService } from '../../services/housing.service';
+import { HousingService } from "../../services/housing.service";
+import { HousingLocation } from "../../models/housing-location";
+
+import { MatCardModule } from "@angular/material/card";
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
-  selector: 'app-details',
-  standalone: true,
-  imports: [],
-  templateUrl: './details.component.html',
-  styleUrl: './details.component.scss'
+	selector: "app-details",
+	standalone: true,
+	imports: [MatCardModule, MatButtonModule, MatIconModule],
+	templateUrl: "./details.component.html",
+	styleUrl: "./details.component.scss",
 })
 export class DetailsComponent {
-  route: ActivatedRoute = inject(ActivatedRoute)
-  housingLocationId = 0;
-  housingService = inject(HousingService);
+	route: ActivatedRoute = inject(ActivatedRoute);
+	housingService = inject(HousingService);
+	housingLocation: HousingLocation | undefined;
 
-  constructor () {
-    this.housingLocationId = Number(this.route.snapshot.params['id']);
-  }
-
+	constructor() {
+		const housingLocationId = Number(this.route.snapshot.params["id"]);
+		this.housingLocation =
+			this.housingService.getHousingLocationById(housingLocationId);
+	}
 }
