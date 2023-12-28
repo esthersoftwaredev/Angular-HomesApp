574b929f1cef7d8fe2db91cf7429c7646f83cc0d
Esther White <monacodelisa@gmail.com>
2023-12-28 12:46:57 +0200

    adding a form mat-form-fields too

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,15 +2,25 @@ import { Component, inject } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { HousingService } from "../../services/housing.service";
 import { HousingLocation } from "../../models/housing-location";
+import { FormControl, FormGroup, ReactiveFormsModule } from "@angular/forms";
 
 import { MatCardModule } from "@angular/material/card";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
 import { MatButtonModule } from "@angular/material/button";
 import { MatIconModule } from "@angular/material/icon";
 
 @Component({
 	selector: "app-details",
 	standalone: true,
-	imports: [MatCardModule, MatButtonModule, MatIconModule],
+	imports: [
+		ReactiveFormsModule,
+		MatFormFieldModule,
+		MatInputModule,
+		MatCardModule,
+		MatButtonModule,
+		MatIconModule,
+	],
 	templateUrl: "./details.component.html",
 	styleUrl: "./details.component.scss",
 })
@@ -19,9 +29,19 @@ export class DetailsComponent {
 	housingService = inject(HousingService);
 	housingLocation: HousingLocation | undefined;
 
+	applyForm = new FormGroup({
+		firstName: new FormControl(""),
+		lastName: new FormControl(""),
+		email: new FormControl(""),
+	});
+
 	constructor() {
 		const housingLocationId = Number(this.route.snapshot.params["id"]);
 		this.housingLocation =
 			this.housingService.getHousingLocationById(housingLocationId);
 	}
+
+  submitApplication() {
+
+  }
 }
