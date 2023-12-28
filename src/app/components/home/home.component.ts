47b99eb59dc8a36709768a09da5a6f447452c24e
Esther White <monacodelisa@gmail.com>
2023-12-28 11:33:06 +0200

    so I've noticed that the constructor is being used instead of ONINit for initial data popilation so I will keep it in the constructor for this app

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, inject } from "@angular/core";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
 import { HousingLocation } from "../../models/housing-location";
 
 import { MatFormFieldModule } from "@angular/material/form-field";
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from "@angular/material/icon";
+import { HousingService } from "../../services/housing.service";
 
 @Component({
 	selector: "app-home",
@@ -20,4 +21,9 @@ import { MatIconModule } from "@angular/material/icon";
 })
 export class HomeComponent {
 	housingLocationList: HousingLocation[] = [];
+  housingService: HousingService = inject(HousingService);
+
+  constructor() {
+    this.housingLocationList = this.housingService.getAllHousingLocations();
+  }
 }
